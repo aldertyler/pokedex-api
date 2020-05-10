ffd098dbc412cda697b4d4ee5a452975c70d3930
Tyler Alder <tyler.b.alder@gmail.com>
2020-05-10 06:23:09 -0700

    added pokedex

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const POKEDEX = require("./pokedex.json");
 
 console.log(process.env.API_TOKEN);
 
 const app = express();
 
 app.use(morgan("dev"));
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+  console.log("validate bearer token middleware");
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+  //move to the next middleware
+  next();
+});
 
 const validTypes = [
   `Bug`,
